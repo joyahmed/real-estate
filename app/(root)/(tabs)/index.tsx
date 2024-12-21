176b16c522a558e06cb4302b2941052ef8f7a0e7
joyahmed <joy_ahmed_007@yahoo.com>
2024-12-21 20:03:30 +0600

    ✅Assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,8 +10,8 @@ export default function Index() {
 				alignItems: 'center'
 			}}
 		>
-			<Text className='font-bold text-lg my-10'>
-				Welcome to ReEstate
+			<Text className='font-bold  my-10 font-rubik text-3xl'>
+				Welcome to Real Estate
 			</Text>
 			<Link href='/sign-in'>Sign In</Link>
 			<Link href='/explore'>Explore</Link>
